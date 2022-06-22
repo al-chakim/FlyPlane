c6663b19824849785e0ed36162487159bd726c9c
Muchammad Amru Al-Chakimi <amrualchakim@gmail.com>
2022-06-22 17:35:32 +0700

    bonus page

--- plane/lib/ui/pages/bonus.dart
+++ plane/lib/ui/pages/bonus.dart
@@ -11,8 +11,77 @@ class Bonus extends StatefulWidget {
 class _BonusState extends State<Bonus> {
   @override
   Widget build(BuildContext context) {
+
+
+    Widget bonusCard(){
+      return Container(
+        width: 300,
+        height: 211,
+        padding: EdgeInsets.all(defaultMargin),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              'assets/mon.png',
+            )
+          ),
+          boxShadow: [BoxShadow(
+            color: ungu.withOpacity(0.5),
+            blurRadius: 50,
+            offset: Offset(0, 10)
+          )
+          ] 
+        ),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Name',
+                        style: whiteTextStyle.copyWith(
+                          fontWeight: medium
+                        ),
+                      ),
+                      Text(
+                        'Al - Chakim',
+                        style: whiteTextStyle.copyWith(
+                          fontSize: 20
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  width: 24,
+                  height: 24,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/logo1.png')
+                    )
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
-      
+      backgroundColor: bg,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              bonusCard(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
