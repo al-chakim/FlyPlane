686a9329896bd11c798e7bc4b966230d067ad3fe
Muchammad Amru Al-Chakimi <amrualchakim@gmail.com>
2022-07-17 11:38:37 +0700

    content detail_page

--- plane/lib/ui/pages/detail_page.dart
+++ plane/lib/ui/pages/detail_page.dart
@@ -42,6 +42,116 @@ class DetailPage extends StatelessWidget {
     );
   }
 
+  Widget Content(){
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.symmetric(
+        horizontal: defaultMargin
+      ),
+      child: Column(
+        children: [
+
+          // gambar emblem
+          Container(
+            width: 110,
+            height: 25,
+            margin: EdgeInsets.only(
+              top: 30,
+            ),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  'assets/emblem.png'
+                )
+              )
+            ),
+          ),
+
+          //title
+          Container(
+            margin: EdgeInsets.only(top: 256),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Lake Ciliwung',
+                        style: whiteTextStyle.copyWith(
+                          fontSize: 24,
+                          fontWeight: semibold
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      Text(
+                        'Tangerang',
+                        style: whiteTextStyle.copyWith(
+                          fontSize: 16,
+                          fontWeight: light
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 18,
+                      width: 18,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/star.png')
+                        )
+                      ),
+                    ),
+                    SizedBox(width: 3,),
+                    Text(
+                      '4.5',
+                      style: whiteTextStyle.copyWith(
+                        fontWeight: semibold
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+
+          // deskripsi
+          Container(
+            margin: EdgeInsets.only(top: 30),
+            width: double.infinity,
+            padding: EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 30
+            ),
+            decoration: BoxDecoration(
+              color: putih,
+              borderRadius: BorderRadius.circular(
+                defaultRadius
+              ),
+            ),
+            child: Column(
+              children: [
+
+                // about adn desc
+                Text(
+                  'About',
+                  style: blackTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: semibold
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
 
     return Scaffold(
       backgroundColor: putih,
@@ -49,6 +159,7 @@ class DetailPage extends StatelessWidget {
         children: [
           BackImage(),
           CustomShadow(),
+          Content(),
         ],
       ),
     );
