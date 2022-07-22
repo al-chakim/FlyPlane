b8ba648a9a9e857fbd005c45939534e04e96018b
Muchammad Amru Al-Chakimi <amrualchakim@gmail.com>
2022-07-22 21:19:11 +0700

    make a model for user

---
+++ plane/lib/cubit/auth_state.dart
@@ -0,0 +1,33 @@
+part of 'auth_cubit.dart';
+
+abstract class AuthState extends Equatable {
+  const AuthState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class AuthInitial extends AuthState {}
+
+class AuthLoading extends AuthState {}
+
+class AuthSuccess extends AuthState {
+
+  final UserModel user;
+
+  AuthSuccess(this.user);
+
+
+  @override
+  List<Object> get props => [user];
+}
+
+class AuthFailed extends AuthState{
+
+  final String eror;
+
+  AuthFailed(this.eror);
+
+  @override
+  List<Object> get props => [eror];
+}
